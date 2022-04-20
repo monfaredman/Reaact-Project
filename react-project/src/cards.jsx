8e360a04d8377d04c15283435fa53ff009f73bbd
moslem <moslemhosseinpour1998@gmail.com>
2022-04-20 08:36:50 +0430

    event handle

--- react-project/src/cards.jsx
+++ react-project/src/cards.jsx
@@ -1,11 +1,39 @@
 import React from "react";
 import "./app.css";
 
-export default function Card(props) {
-  return (
-    <div className="card">
-      <h1>{props.name}</h1>
-      <p>{props.age}</p>
-    </div>
-  );
+// export default function Card() {
+//   function handleClick() {
+//     console.log(25);
+//   }
+//   const x = (e) => console.log(e);
+//   return (
+//     <div>
+//       <h1>Hi MonfaredMan</h1>
+//       <button onClick={x}>click me</button>
+//     </div>
+//   );
+// }
+
+export default class ToggleBtn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hi MonfaredMan</h1>
+        <p>{this.state.count}</p>
+        <button onClick={this.handleClick}>click me</button>{" "}
+      </div>
+    );
+  }
+  handleClick() {
+    this.setState((state) => {
+      return { count: state.count + 1 };
+    });
+  }
 }
--- react-project/src/index.js
+++ react-project/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Card from "./cards";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Card />, document.getElementById("root"));
