ea3bc7a4f1a47877b596fea384ee24853cf06526
moslem <moslemhosseinpour1998@gmail.com>
2022-06-28 09:25:34 +0430

    edit--files

--- src/component/cardDes.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "../app.css";
-export default function CardDes(props) {
-  return (
-    <div className="clash-card__unit-description">{props.data.description}</div>
-  );
-}
--- src/component/cardImage.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "../app.css";
-export default function CardImage(props) {
-  return (
-    <div className="clash-card__image clash-card__image--barbarian">
-      <img src={props.data.src} alt="barbarian" />
-    </div>
-  );
-}
